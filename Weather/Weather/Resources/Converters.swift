0333bb5cddc6b0a7cb78c7e2863e3fffba992251
agapkinadiana <agapkiandiana@mail.ru>
2020-10-13 14:23:00 +0300

    add: converters + assets

---
+++ Weather/Weather/Resources/Converters.swift
@@ -0,0 +1,32 @@
+//
+//  Converters.swift
+//  Weather
+//
+//  Created by Diana Agapkina on 10/13/20.
+//
+
+import Foundation
+
+struct Converters {
+    
+    static func getDayFromDate(_ date: Date?) -> String {
+        
+        guard let inputDate = date else { return ""}
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: inputDate)
+    }
+    
+    static func getTimeFromDate(_ date: Date?) -> String {
+        
+        guard let inputDate = date else { return ""}
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter.string(from: inputDate)
+    }
+    
+    static func convertToCelsius(_ fahrenheit: Double) -> Int {
+        return Int(5.0 / 9.0 * (Double(fahrenheit) - 32.0))
+    }
+    
+}
